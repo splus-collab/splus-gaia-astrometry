e4c314ae46085cb18941faa596c4b2fc312d8434
herpichfr <fabiorafaelh@gmail.com>
2023-07-31 12:29:13 +0100

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+S-PLUS - Gaia astrometric comparison
+====================================
+"""
+
+__version__ = '0.3'
+__author__ = 'Fabio R Hepich'
+__credits__ = ['Fabio R Hepich', 'S-PLUS Collaboration']
